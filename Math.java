e05ebc6233e7aad66c621870efe83d7d4e94e0d1
Jens Lindström <jl_3570@hotmail.com>
2019-12-03 11:18:43 +0100

    made some changes

--- Math.java
+++ Math.java
@@ -6,7 +6,7 @@ public class Math {
         return first + second;
     }
 
-    public int sub(int first, int second) {
-        return first - second;
+    public int sub(int third, int fifth) {
+        return third   - fifth;
     }
 }
