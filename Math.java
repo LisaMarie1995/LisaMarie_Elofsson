9192ce2f44332b849d8288ce3826e97125eead41
LisaMarie1995 <57871546+LisaMarie1995@users.noreply.github.com>
2019-12-03 11:20:07 +0100

    Changes to sub class

--- Math.java
+++ Math.java
@@ -6,7 +6,7 @@ public class Math {
         return first + second;
     }
 
-    public int sub(int first, int second) {
-        return first - second;
+    public int sub(int fith, int sixth) {
+        return fith - sixth;
     }
 }
